c170483ac7f33af674809ad8587f04d6d50d788b
overlord-destro <edemcoffie41@gmail.com>
2022-11-29 18:30:37 +0000

    Added function that converts string to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in range(len(str)):
+        if ord(str[i]) >= 97 and ord(str[i]):
+            print("{:c}".format(ord(str[i]) - 32), end="")
+        else:
+            print(str[i], end="")
