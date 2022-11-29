b998fdef75046019faa592879796c36a521d5293
overlord-destro <edemcoffie41@gmail.com>
2022-11-29 12:47:09 +0000

    Fixed pcodestyle errors, checking to see if it works

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number >= 0:
-        print(number % 10, end = "")
+        print(number % 10, end="")
         return (number % 10)
     elif number < 0:
-        print(-(number) % 10, end = "")
+        print(-(number) % 10, end="")
         return (-(number) % 10)
-
-r = print_last_digit(0)
-print(r)
