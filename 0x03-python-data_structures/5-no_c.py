c0c37050ccdaf022bb45e23c18c8ac75886406c3
overlord-destro <edemcoffie41@gmail.com>
2022-12-01 17:37:45 +0000

    Added function that removes elements from string

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    newstring = ""
+    for i in range(len(my_string)):
+        if my_string[i] == "c" or my_string[i] == "C":
+            pass
+        else:
+            newstring += my_string[i]
+    return newstring
