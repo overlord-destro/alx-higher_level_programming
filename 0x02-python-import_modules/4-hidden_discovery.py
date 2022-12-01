98428b16346074ad1bba84368358a98537a0fa1f
overlord-destro <edemcoffie41@gmail.com>
2022-12-01 12:24:58 +0000

    .

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import hidden_4
-newl = dir(hidden_4)
-for i in range(len(newl)):
-    if newl[i][0] != "_":
-        print(newl[i])
-    else:
-        pass
+if __name__ == "__main__":
+    import hidden_4
+    newl = dir(hidden_4)
+    for i in range(len(newl)):
+        if newl[i][0] != "_":
+            print(newl[i])
+        else:
+            pass
