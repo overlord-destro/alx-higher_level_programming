9f54754ab7590d4fd63f9012d06c1dfcbc16d578
overlord-destro <edemcoffie41@gmail.com>
2022-12-01 20:43:57 +0000

    Added function that retruns largest value in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if my_list == []:
+        return None
+
+    for i in range(len(my_list)):
+        temp = my_list[i]
+        if my_list[i] > temp:
+            temp = my_list[i]
+    return temp
