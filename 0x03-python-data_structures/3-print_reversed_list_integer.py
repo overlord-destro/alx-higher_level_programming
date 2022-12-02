9731ce79c0d85021dbf54ee270139bab2942d212
overlord-destro <edemcoffie41@gmail.com>
2022-12-02 18:22:59 +0000

    Added differnet reversal method

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    n = len(my_list) - 1
-    while n >= 0:
-        print("{:d}".format(my_list[n]))
-        n -= 1
-
-my_list = [1, 2, 3, 4, 5]
-print_reversed_list_integer(my_list)
+    my_list.reverse()
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
