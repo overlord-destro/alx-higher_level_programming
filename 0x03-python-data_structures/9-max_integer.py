7795fdce641b3ee39fd3ae978f5ae1cec329b950
overlord-destro <edemcoffie41@gmail.com>
2022-12-03 15:58:44 +0000

    removed assignment of first temp value from loop

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,9 +2,8 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-
+    temp = my_list[0]
     for i in range(len(my_list)):
-        temp = my_list[0]
         if my_list[i] > temp:
             temp = my_list[i]
     return temp
