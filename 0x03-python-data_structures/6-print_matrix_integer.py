ba348e5b722771fe93dc962f09b4cf9fc51d89c1
overlord-destro <edemcoffie41@gmail.com>
2022-12-03 18:31:02 +0000

    Made some changes to matrix printer

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            print("{:d}".format(matrix[i][j]), end=" ")
+            print("{:d}".format(matrix[i][j]), end="")
+            if matrix[i][j] == matrix[i][len(matrix[i]) - 1):
+                continue
+            print("", end=" ")
         print("")
