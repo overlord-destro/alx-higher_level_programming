e29b8ebefcbaf361ac90866c0018910d8af233fd
overlord-destro <edemcoffie41@gmail.com>
2022-12-07 13:17:41 +0000

    Added function that prints dictionary values by sorted keys

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@ def print_sorted_dictionary(a_dictionary):
     newlist = list(a_dictionary)
     newlist.sort()
     for i in newlist:
-        print("{:s}: {}".format(i, a_dictionary))
+        print("{:s}: {}".format(i, a_dictionary[i]))
