06c6a81085a0c9d7782f9a78e5ec46ff8071eb43
overlord-destro <edemcoffie41@gmail.com>
2022-12-07 13:21:28 +0000

    Fixed pycodestyle

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -5,10 +5,3 @@ def update_dictionary(a_dictionary, key, value):
     else:
         a_dictionary.setdefault(key, value)
     return a_dictionary
-
-
-#a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
-#new_dict = update_dictionary(a_dictionary, 'language', "Python")
-#print(new_dict)
-#print("--------")
-#print(a_dictionary)
