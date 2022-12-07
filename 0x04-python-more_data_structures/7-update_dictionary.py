c6a6d152ebd7b4bbe03daff92f2f873b0339d16d
overlord-destro <edemcoffie41@gmail.com>
2022-12-07 12:18:59 +0000

    Added function that updates key in dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary.update({key: value})
+    if a_dictionary:
+        a_dictionary.update({key: value})
     return a_dictionary
+
+
+#a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+#new_dict = update_dictionary(a_dictionary, 'language', "Python")
+#print(new_dict)
+#print("--------")
+#print(a_dictionary)
