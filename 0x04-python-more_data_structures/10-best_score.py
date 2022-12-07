017399efaf5d2e1eee64486d2ed8a60a3fe87429
overlord-destro <edemcoffie41@gmail.com>
2022-12-07 13:51:19 +0000

    added brackets to .items

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    temp_value = 0
+    for 
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     newdict = {}
-    for keys, vals in a_dictionary.items:
-        newdict[keys] = vals * 2
+    for keys, vals in a_dictionary.items():
+        newdict.setdefault(keys, vals * 2)
     return newdict
