c81992a0a53589c1131cb6ca63159efacd645a6f
overlord-destro <edemcoffie41@gmail.com>
2022-12-20 20:53:47 +0000

    Added class that has private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""creating a class square that defines a square"""
+
+
+class Square:
+    """class square with private attribute"""
+    def __init__(self, __size):
+        self.__size = __size
