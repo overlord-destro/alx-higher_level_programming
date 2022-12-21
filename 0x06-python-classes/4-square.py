636106489cd7c12de460002cf5eeab410d13dd72
overlord-destro <edemcoffie41@gmail.com>
2022-12-21 10:23:57 +0000

    Added class that has getter and etter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Creating a class Square that defines a square"""
+
+
+class Square:
+    """creating a class square"""
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
+    @property
+    def size(self):
+        return self.__size
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
