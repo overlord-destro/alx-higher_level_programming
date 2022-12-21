b4cbc261fe761d14092bcfd24a34f65b38135571
overlord-destro <edemcoffie41@gmail.com>
2022-12-21 10:49:04 +0000

    Added square class that prints square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -27,6 +27,8 @@ class Square:
         self.__size = value
 
     def my_print(self):
+        if self.__size == 0:
+            print()
         for i in range(self.__size):
             for j in range(self.__size):
                 print("#", end="")
