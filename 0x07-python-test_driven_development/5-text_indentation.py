f6c94531f7c781a12bcb43ba7fa32d8e9ab1041e
overlord-destro <edemcoffie41@gmail.com>
2022-12-30 04:06:14 +0000

    Added function that adds 2 new lines after punctuation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
+"""Function to  print text with 2 new lines after certain characters"""
+
+
 def text_indentation(text):
+    """
+    Function that prints 2 new lines after ['.', ':', '?'] and accepts
+    only strings else will raise a type errorr
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    for char in text:
+
+    new = ""
+    for i in range(len(text)):
+        if text[i] == " " and text[i - 1] in [".", ":", "?"]:
+            continue
+        else:
+            new += text[i]
+
+    for char in new:
         if char in [".", "?", ":"]:
-            print(char + "\n",sep="")
+            print(char + "\n\n", end="")
         else:
             print(char, end="")
