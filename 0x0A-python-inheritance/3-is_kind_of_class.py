a626ef6250a8a7406caba3ba1205498184ef9e7c
overlord-destro <edemcoffie41@gmail.com>
2023-01-09 11:59:20 +0000

    Added function that checks if object is instance of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Function that returns true if object is an instance of inherited class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Function that returns true if object is an instance of inherited class"""
+    if isinstance(obj, a_class) or isinstance(a, object):
+        return True
+    else:
+        return False
