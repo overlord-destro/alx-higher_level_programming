f5265d4fd565c933c37b73f65ec32603b5a09c74
overlord-destro <edemcoffie41@gmail.com>
2023-01-09 12:48:35 +0000

    Added public instance method to class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A class with public instance attribute area"""
+
+
+class BaseGeometry():
+    """A class with public instance attribute area"""
+    def area(self):
+        raise Exception("area() is not implemented")
