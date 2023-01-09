8bffb5d26bc1a5210b5d9966a0459785a8fcd478
overlord-destro <edemcoffie41@gmail.com>
2023-01-09 12:35:17 +0000

    .

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""function that returns true if object is an instance of class"""
+
+
 def inherits_from(obj, a_class):
     """
     function that returns true if object is an instance of class
