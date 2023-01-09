e4e70e732b1f1d195f6ed5b09fe5cacfb670c71a
overlord-destro <edemcoffie41@gmail.com>
2023-01-09 15:10:34 +0000

    Added square class

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Class square that ingerits from Rectnagel"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """child class of Rectangle"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
