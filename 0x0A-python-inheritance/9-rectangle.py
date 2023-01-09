022b3e04a8470761e307fa6813803e1170e6e958
overlord-destro <edemcoffie41@gmail.com>
2023-01-09 15:00:28 +0000

    Added subcalss of basegeometry

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def area(self):
-        return width * height
+        return self.__width * self.__height
 
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
