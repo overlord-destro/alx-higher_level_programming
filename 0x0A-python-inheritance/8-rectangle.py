e8961ea12344b51930be77e0bdac5bc6d8212209
overlord-destro <edemcoffie41@gmail.com>
2023-01-09 14:49:10 +0000

    Added subcalss of basegeometry

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class that inherits from BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """sub class of BaseGeometry"""
+    def __init__(self, width, height):
+        BaseGeometry.integer_validator("width", width)
+        BaseGeometry.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
