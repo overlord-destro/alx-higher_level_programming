ae3fbfbfafea319a7950f4c811b08127fcc038cd
overlord-destro <edemcoffie41@gmail.com>
2023-01-10 11:33:46 +0000

    Added function that returns python object from json string

---
+++ 0x0B-python-input_output/4-from_json-string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function that returns python obj from JSON"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """returns python data structure from json string"""
+    return json.loads(my_obj)
