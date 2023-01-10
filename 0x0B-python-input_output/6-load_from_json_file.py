4a749b9dad253bb921c4b80f4ae5ec24384f9410
overlord-destro <edemcoffie41@gmail.com>
2023-01-10 12:40:04 +0000

    Added function that creates object from json str

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,3 +6,7 @@ import json
 
 
 def load_from_json_file(filename):
+    """creates an object from json file"""
+    with open(filename, "r", encoding="utf-8") as f:
+        jsonstr = f.read()
+    return json.loads(jsonstr)
