bca95164f7ad6343c02c181bfe767bce9b4ccbfa
overlord-destro <edemcoffie41@gmail.com>
2023-01-10 12:30:16 +0000

    Removed mode

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """Function to print content of file to stdout"""
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         outp = f.read()
         print(outp)
