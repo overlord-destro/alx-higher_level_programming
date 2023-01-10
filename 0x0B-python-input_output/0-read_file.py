06ca3e3684c7700eeda5fea7dee4750bb187f79a
overlord-destro <edemcoffie41@gmail.com>
2023-01-10 12:33:25 +0000

    Removed line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """Function to print content of file to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
