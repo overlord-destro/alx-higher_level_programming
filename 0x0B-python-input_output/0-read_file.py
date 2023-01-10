1564a3d77219f8dfe95aa81fdcce8d95320164b9
overlord-destro <edemcoffie41@gmail.com>
2023-01-10 12:29:43 +0000

    Put mode in quotes

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """Function to print content of file to stdout"""
     with open(filename, "r", encoding="utf-8") as f:
         outp = f.read()
-    print(outp)
+        print(outp)
