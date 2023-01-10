a34a4ee8244f87fe8488eea890a7ff3f61106e7d
overlord-destro <edemcoffie41@gmail.com>
2023-01-10 11:07:40 +0000

    Added function that reads from file and prints to stdout

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Function that reads file and prints to stdout"""
+
+
+def read_file(filename=""):
+    """Function to print content of file to stdout"""
+    with open(filename, r, encoding="utf-8") as f:
+        outp = f.read()
+    print(outp)
