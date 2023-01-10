ff5b311c8f6b1f9e1da17370e2b0f700e4e4bd40
overlord-destro <edemcoffie41@gmail.com>
2023-01-10 12:18:18 +0000

    changed how i converted string

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,4 +8,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """writes an object to file using JSON rep"""
     with open(filename, "w", encoding="utf-8") as f:
-        f.write(json.dumps(newobj))
+        f.write(json.dumps(my_obj))
